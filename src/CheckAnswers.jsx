c96d87c3e3354555496e6a38a1177bad6de2102e
Bojan Lukic <bojan.ognjen@gmail.com>
2025-08-29 00:33:55 +0200

    Adding CheckAnswers component

---
+++ src/CheckAnswers.jsx
@@ -0,0 +1,6 @@
+export default function CheckAnswers() {
+    console.log(gameStarted)
+    return(
+        <button className="check button" onClick={checkingAnswers}>Check answers</button>
+    )
+}
